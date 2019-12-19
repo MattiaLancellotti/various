5136fc26a021c2dce81bbdc78acd6028865c544c
m <mattialancellotti@protonmail.com>
2019-12-19 22:19:58 +0100

    Random programs

---
+++ GraphPath/main.c
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define ROWS 6
+#define COLUMNS 6
+#define NOT_CONNECTED -1
+
+enum CITIES { A, B, C, D, E, F };
+
+//  ###  A  B  C  D  E  F
+//   A   0 50 -1 -1 -1 70
+//   B  50  0 40 -1 50 -1
+//   C  -1 40  0 20 20 -1
+//   D  -1 -1 20  0 -1 -1
+//   E  -1 20 50 -1  0 50
+//   F  70 -1 -1 -1 50  0
+
+void print_mat(int[][COLUMNS]);
+char *print_path(int [], size_t);
+int are_connected(int[][COLUMNS], int, int);
+int are_interconnected(int[][COLUMNS], int[], int, int, int);
+static void shift(int[], size_t);
+static void reset(int[], size_t);
+
+int main() {
+	int mat[ROWS][COLUMNS] = { 0 }, path[COLUMNS] = { -1, -1, -1, -1, -1, -1 };
+
+	mat[A][A] = 0;
+	mat[A][B] = 50;
+	mat[A][C] = NOT_CONNECTED;
+	mat[A][D] = NOT_CONNECTED;
+  mat[A][E] = NOT_CONNECTED;
+
+	mat[B][A] = 50;
+	mat[B][B] = 0;
+	mat[B][C] = 40;
+	mat[B][D] = NOT_CONNECTED;
+  mat[B][E] = 20;
+
+	mat[C][A] = NOT_CONNECTED;
+	mat[C][B] = 40;
+	mat[C][C] = 0;
+	mat[C][D] = 20;
+  mat[C][E] = 50;
+
+	mat[D][A] = NOT_CONNECTED;
+	mat[D][B] = NOT_CONNECTED;
+	mat[D][C] = 20;
+	mat[D][D] = 0; 
+  mat[D][E] = NOT_CONNECTED;
+  
+  mat[E][A] = NOT_CONNECTED;
+  mat[E][B] = 20;
+  mat[E][C] = 50;
+  mat[E][D] = NOT_CONNECTED;
+  mat[E][E] = NOT_CONNECTED;
+
+  mat[F][A] = 70;
+  mat[F][B] = NOT_CONNECTED;
+  mat[F][C] = NOT_CONNECTED;
+  mat[F][D] = NOT_CONNECTED;  
+  mat[F][E] = 50;
+  mat[F][F] = 0;
+
+	print_mat(mat);
+	printf("Is there a direct way?    A -> B: %d\n", are_connected(mat, A, B) ? 1 : 0);
+	printf("Is there a direct way?    A -> D: %d\n", are_connected(mat, A, D) ? 1 : 0);
+  printf("Is there a direct way?    A -> E: %d\n", are_connected(mat, A, E) ? 1 : 0);
+  printf("Is there a direct way?    E -> C: %d\n", are_connected(mat, E, C) ? 1 : 0);
+  printf("Is there a direct way?    F -> E: %d\n", are_connected(mat, F, E) ? 1 : 0);
+  printf("Is there a direct way?    F -> D: %d\n", are_connected(mat, F, D) ? 1 : 0);
+
+	printf("Is there an indirect way? E -> E: %d\n", are_interconnected(mat, path, E, E, A));
+	printf("Path: \n=> ");
+  printf("%s", print_path(path, COLUMNS));
+
+  reset(path, COLUMNS);
+  
+  printf("Is there an indirect way? D -> A: %d\n", are_interconnected(mat, path, D, D, A));
+  printf("Path: \n=> ");
+  printf("%s", print_path(path, COLUMNS));
+
+  reset(path, COLUMNS);
+
+  printf("Is there an indirect way? F -> D: %d\n", are_interconnected(mat, path, F, F, D));
+  printf("Path: \n=> ");
+  printf("%s", print_path(path, COLUMNS));
+
+  reset(path, COLUMNS);
+
+  printf("Is there an indirect way? F -> C: %d\n", are_interconnected(mat, path, F, F, C));
+  printf("Path: \n=>");
+  printf("%s", print_path(path, COLUMNS));
+}
+
+void print_mat(int mat[][COLUMNS]) {
+	printf("CONNECTIONS:\n");
+	for (size_t i=0; i<ROWS; i++) {
+		for (size_t j=0; j<COLUMNS; j++)
+			printf("\t> From location %2ld to location %2ld: %3d Km\n", i, j, mat[i][j]);
+
+    if (i != ROWS-1)
+      printf("\n");
+  }
+}
+
+int are_connected(int mat[][COLUMNS], int first_location, int second_location) {
+	return first_location != -1 && second_location != -1 ? mat[first_location][second_location]+1 : 0;
+}
+
+int are_interconnected(int mat[][COLUMNS], int path[], int start_location, int first_location, int second_location) {
+	int first_locations_connections[COLUMNS];
+
+	if (first_location == second_location) {
+		path[0] = first_location;
+		return 1;
+	}
+
+	//getting nodes
+	for (int i=0; i<COLUMNS; i++)
+		first_locations_connections[i] = mat[first_location][i] > 0 ? i : -1;
+
+	//getting deeper
+	for (size_t i=0; i<COLUMNS; i++)
+		if (first_locations_connections[i] != -1 && start_location != first_locations_connections[i])
+			if (are_interconnected(mat, path, first_location, first_locations_connections[i], second_location)) {
+				shift(path, COLUMNS);
+			  path[0] = first_location;
+				return 1;
+			}
+
+	return 0;
+}
+
+static void shift(int vet[], size_t size) {
+	for (size_t i=size-1; i>0; i--)
+		vet[i] = vet[i-1];
+	vet[0] = 0;
+}
+
+static void reset(int vet[], size_t size) {
+  for (size_t i=0; i<size; i++)
+    vet[i] = -1;
+}
+
+char *print_path(int path[], size_t size) {
+  char *tmp = malloc(sizeof(char)*23);
+
+  strcpy(tmp, "{ ");
+	for (size_t i=0; i<size; i++)
+		switch(path[i]) {
+      case A:
+        strcat(tmp, "A ");
+        break;
+      case B:
+        strcat(tmp, "B ");
+        break;
+      case C:
+        strcat(tmp, "C ");
+        break;
+      case D:
+        strcat(tmp, "D ");
+        break;
+      case E:
+        strcat(tmp, "E ");
+        break;
+      case F:
+        strcat(tmp, "F ");
+        break;
+      default:
+        break;
+    }
+
+  strcat(tmp, "}\n\0");
+  return realloc(tmp, strlen(tmp));
+}
---
+++ boh/main.c
@@ -0,0 +1,252 @@
+#include<stdlib.h> 
+#include<stdio.h> 
+#include<unistd.h> 
+#include<time.h> 
+#include<ctype.h>
+#include<sys/ioctl.h>
+
+#define SIDE 10
+#define BOX 8
+
+#define CLEAR system("clear");
+#define RAW system("stty raw");
+#define NORAW system("stty -raw");
+#define NOECHO system("stty -echo");
+#define ECHO system("stty echo");
+#define Up(x); printf("\033[%dA", x);
+#define Down(x); printf("\033[%dB", x);
+#define Right(x); printf("\033[%dC", x);
+#define Left(x); printf("\033[%dD", x);
+
+static struct winsize w;
+
+static struct {
+	int selected;
+	int xpos, ypos;
+} *cursor;
+
+struct game_box {
+	int **content;
+	int name;
+	int x, y;
+	int layer;
+};
+
+static int *ptr;
+
+// Help
+void help(void);
+void game_help(void);
+
+// Init
+int init_game(struct game_box **);
+void init_menu(int);
+void load(int **);
+
+// Mem
+struct game_box *new_gbox(int, int, int, int); //not freed
+
+// Input
+int handle_input(struct game_box **, int);
+
+// GUI/TTY
+void draw(struct game_box *);
+void set_mouse(int, int);
+int check(struct game_box **);
+static void draw_h_line(int);
+static void draw_v_line(int **, struct game_box *);
+
+// Support
+int search(struct game_box **, int);
+
+int main() {
+	struct game_box **gwin = malloc(sizeof(struct game_box *)*BOX);
+
+	CLEAR
+	if (!init_game(gwin))
+		return 0;
+	RAW
+	NOECHO
+	while(handle_input(gwin, cursor->selected))
+		;
+	NORAW
+	ECHO
+	CLEAR
+
+	free(cursor);
+	free(ptr);
+
+	return 0;
+}
+
+int init_game(struct game_box **gwin) {
+	int i, c=10;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+
+	for (i=0; i<BOX; i++) {
+		gwin[i] = new_gbox(i, c, 10, 2);
+		load(gwin[i]->content);
+		c+=SIDE+5;
+	}
+
+	if (check(gwin))
+		return 0;
+
+	cursor = malloc(sizeof(void *));
+	cursor->selected = 0;
+	cursor->xpos=0;
+	cursor->ypos=0;
+
+	ptr = malloc(sizeof(int)*3);
+	ptr[0] = gwin[0]->content[0][0]; //content, useless
+	ptr[1] = 0; // x
+	ptr[2] = 0; // y
+	for (i=0; i<BOX; i++)
+		draw(gwin[i]);
+
+	return 1;
+}
+
+struct game_box *new_gbox(int name, int new_x, int new_y, int new_layer) {
+	struct game_box *new = malloc(sizeof(struct game_box));
+	new->content = malloc(sizeof(int *)*SIDE);
+	new->name = name;
+	new->x = new_x;
+	new->y = new_y;
+	new->layer = new_layer;
+	return new;
+}
+
+int handle_input(struct game_box **gwin, int current) {
+	int c, tmp, i;
+	struct game_box *win = gwin[current];
+
+	set_mouse(win->x+1, win->y+1);
+	ptr[0] = win->content[0][0];
+	ptr[1] = 0;
+	ptr[2] = 0;
+	while((c=getchar()) != 'q')
+		if (c == 'l') 
+			if (ptr[1] == SIDE-1) {
+				set_mouse(win->x+1, cursor->ypos);
+				ptr[0] = win->content[0][ptr[2]];
+				ptr[1] = 0;
+			} else {
+				set_mouse(cursor->xpos+1, cursor->ypos);
+				ptr[0] = win->content[++ptr[1]][ptr[2]];
+			}
+		else if (c == 'h')
+			if (ptr[1] == 0) {
+				set_mouse(win->x+SIDE, cursor->ypos);
+				ptr[0] = win->content[SIDE-1][ptr[2]];
+				ptr[1] = SIDE-1;
+			} else {
+				set_mouse(cursor->xpos-1, cursor->ypos);
+				ptr[0] = win->content[--ptr[1]][ptr[2]];
+			}
+		else if (c == 'j')
+			if (ptr[2] == SIDE-1) {
+				set_mouse(cursor->xpos, win->y+1);
+				ptr[0] = win->content[ptr[1]][0];
+				ptr[2] = 0;
+			} else {
+				set_mouse(cursor->xpos, cursor->ypos+1);
+				ptr[0] = win->content[ptr[1]][++ptr[2]];
+			}
+		else if (c == 'k')
+			if (ptr[2] == 0) {
+				set_mouse(cursor->xpos, win->y+SIDE);
+				ptr[0] = win->content[ptr[1]][SIDE-1];
+				ptr[2] = SIDE-1;
+			} else {
+				set_mouse(cursor->xpos, cursor->ypos-1);
+				ptr[0] = win->content[ptr[1]][--ptr[2]];
+			}
+		else if (c == 32) {
+			if (win->content[ptr[2]][ptr[1]] < 48)
+				(win->content[ptr[2]][ptr[1]])++;
+			else
+				win->content[ptr[2]][ptr[1]] = 40;
+			CLEAR
+			set_mouse(0, 0);
+			for (i=0; i<BOX; i++)
+				draw(gwin[i]);
+			set_mouse(win->x+1+ptr[1], win->y+1+ptr[2]);
+		} else if (isdigit(c)) {
+				tmp = search(gwin, c-48);
+				if (tmp != -1) {
+					cursor->selected = tmp;
+					return 1;
+				}
+		}
+	return 0;
+}
+
+void load(int **c) {
+	int i, j;
+	srand(time(NULL));
+	for (i=0; i<SIDE; i++) {
+		*(c+i) = malloc(sizeof(int)*SIDE);
+		for(j=0; j<SIDE; j++)
+			*(*(c+i)+j) = rand()%10+40;
+	}
+}
+
+int check(struct game_box **gwin) {
+	if (gwin[BOX-1]->x+SIDE+2 > w.ws_col || gwin[BOX-1]->y+SIDE+2 > w.ws_row)
+		return 1;
+	return 0;
+}
+
+void draw(struct game_box *win) {
+	set_mouse(win->x, win->y);
+	draw_h_line(0);
+	draw_v_line(win->content, win);
+	set_mouse(win->x, cursor->ypos+1);
+	draw_h_line(1);
+}
+
+void set_mouse(int x, int y) {
+	if (y > cursor->ypos) {
+		Down(y-cursor->ypos);
+	} else if (y < cursor->ypos) {
+		Up(cursor->ypos-y);
+	}
+	cursor->ypos = y;
+
+	if (x > cursor->xpos) {
+		Right(x-cursor->xpos);
+	} else if (x < cursor->xpos) {
+		Left(cursor->xpos-x);
+	}
+	cursor->xpos = x;
+}
+
+static void draw_h_line(int v) {
+	int i=0;
+	printf(v ? "\u2517" : "\u250F");
+	while(i++<SIDE)
+		printf("\u2501");
+	printf(v ? "\u251B" : "\u2513");
+	cursor->xpos+= SIDE+2;
+}
+
+static void draw_v_line(int **c, struct game_box *win) {
+	int i, j;
+	for (i=0; i<SIDE; i++) {
+		set_mouse(win->x, cursor->ypos+1);
+		printf("\u2503");
+		for (j=0; j<SIDE; j++)
+			printf("\033[%dm \033[0m", c[i][j]);
+		printf("\u2503");
+		cursor->xpos += SIDE+2;
+	}
+}
+
+int search(struct game_box **gwin, int name) {
+	int i;
+	for(i=0; i<BOX; i++)
+		if (gwin[i]->name == name)
+			return i;
+	return -1;
+}
