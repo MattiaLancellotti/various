7f2946fc9588c14dbbf9424355da0b2a39f9f406
mattia <lancellottimattia@protonmail.com>
2022-03-19 16:45:15 +0100

    Better comments

--- nasm/Hello-World/hello-world.asm
+++ nasm/Hello-World/hello-world.asm
@@ -9,16 +9,22 @@ start:
      mov bx, string             ; Loading into the BX register the address
                                 ; of 'string'
 repeat:
-     mov al, [bx]               ; Load a byte in AL from address pointed by BX
-     test al, al                ; Tests AL for zero
-     je end                     ; If the test is zero jump to 'end'
-     push bx                    ; Save BX register in stack
-     mov ah, 0x0e               ; Load AH with code for terminal output
-     mov bx, 0x000f             ; BH is page zero, BL is color (graphic mode)
-     int 0x10                   ; Call the BIOS for displaying one letter
-     pop bx                     ; Restore BX register from stack
-     inc bx                     ; Increase the BX register by 1
-     jmp repeat                 ; Jumps to 'repeat'
+     mov al, [bx]               ; This block of code first copies the address of
+     test al, al                ; what's in BX to AL (in our case the current
+     je end                     ; character from the 'string'), than logically
+                                ; tests (using & for each bit) if the given
+                                ; character is 0. If it is than jumps to the
+                                ; end because the string ended.
+
+     push bx                    ; If the character wasn't 0, push it onto the
+     mov ah, 0x0E               ; stack 'cause we need space in the registers to
+     mov bx, 0x00F              ; call a BIOS routine. Than loads registers with
+     int 0x10                   ; the right termnal codes and calls the BIOS'
+                                ; routine.
+
+     pop bx                     ; Restore BX register with the value that was
+     inc bx                     ; previously pushed on the stack, increments it
+     jmp repeat                 ; and reload jumps back to 'repeat'.
 
 end:
      int 0x20                   ; Calls the exit command
