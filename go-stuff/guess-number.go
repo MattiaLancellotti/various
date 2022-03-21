06679d9d73d4a69b6ff89c30320f123d799d0f0d
root <lancellottimattia@protonmail.com>
2022-03-21 11:10:41 +0000

    Generation algorithm has its own function

--- go-stuff/guess-number.go
+++ go-stuff/guess-number.go
@@ -6,35 +6,33 @@ import (
      "math/rand"
 )
 
-// TODO: Put the random number generator in its own function.
+// This function takes an integer as the limit of the pseudo-random generation.
+func GenerateNumber(limit int) int {
+     fmt.Println("Generating a random number between 0 and ", limit)
+
+     // Using the time as seed for the algorithm.
+     seed := rand.NewSource(time.Now().UnixNano())
+     randObj := rand.New(seed)
+
+     return randObj.Intn(limit)
+}
+
 // TODO: Maybe some pretty printing.
 func main() {
-     // Parsing milliseconds
-     m, err := time.ParseDuration("2s")
-     if err != nil {
-          fmt.Println("An error has occurred while parsing time.")
-     }
-
-     // Waiting m seconds
-     fmt.Println("Now choosing a number ")
-     time.Sleep(m)
-
-     // Choosing the random number
-     s1 := rand.NewSource(time.Now().UnixNano())
-     r1 := rand.New(s1)
-     num := r1.Intn(10)
+     // Generating the new number
+     number := GenerateNumber(10)
 
      // Time for the user to guess the number
      var userGuess int
      for {
           fmt.Print("Your turn: ")
-          _, err = fmt.Scan(&userGuess)
+          _, err := fmt.Scan(&userGuess)
           if err != nil {
                fmt.Println("Something went wrong while reading user's input.")
           }
 
           // Checking if the user guessed the number
-          if userGuess == num { break }
+          if userGuess == number { break }
 
           fmt.Println("Gotta try again mate!")
      }
